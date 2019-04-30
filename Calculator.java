39e5349750b0bd40da03ab50ca22ac20aa9f4e42
michael-chris <49767195+michael-chris@users.noreply.github.com>
2019-04-30 15:37:46 -0500

    Add files via upload
    Adding Listeners to The Buttons

--- Calculator.java
+++ Calculator.java
@@ -1,51 +1,89 @@
+import java.awt.Color;
+
 import javax.swing.*;
 
 public class Calculator extends JFrame{
-	private int x;
-	private int y;
-	private int answer;
 	
-	//public int getX();
+    public static final int WIDTH = 600, HEIGHT = 800;
+    
+    private Numpad num;
+    private Display dis;
+    
+	private String x;
+	private String y;
+	private String answer;
 	
-	
-	public int getX() {
-		return x;
-	}
-	
-	public int getY() {
-		return y;
-	}
-	
-	
-	public int getAnswer() {
-		return answer;
-	}
-	
-	//Adds Two numbers together and returns answer;
-	public int add(int x, int y) {
-		return x + y;
-	}
-	
-	//Subtracts Two Numbers together and returns answer;
-	public int sub(int x, int y) {
-		return x - y;
-	}
-	
-	//Multiplies Two numbers together and returns answer;
-	public int multi(int x, int y) {
-		return x * y;
-	}
-	
-	//Divides Two Numbers Together and Returns answer, if divided by zero throws error;
-	public int div(int x, int y) {
-		try {
-			answer = x / y;
-		}
-		catch (ArithmeticException ae){
-			System.out.println("Divide by 0 Error!");
-			return 0;
-		}
-		return answer;
+	public Calculator(){
 		
+		super("Calculator");
+		
+		
+        this.setSize(WIDTH,HEIGHT);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setResizable(false);
+        this.setLayout(null); 
+        this.setLocationRelativeTo(null);
+        
+        
+        this.num = new Numpad();
+        this.num.setSize(Calculator.WIDTH, 3 * Calculator.HEIGHT/4);
+        this.num.setBounds(0, Calculator.HEIGHT / 5, Calculator.WIDTH, 3 * Calculator.HEIGHT/4);
+        this.add(num);
+        
+        
+        this.dis = new Display();
+        this.dis.setSize(Calculator.WIDTH, Calculator.HEIGHT/4);
+        this.dis.setBounds(Calculator.WIDTH/20,Calculator.WIDTH/20,Calculator.WIDTH, Calculator.HEIGHT/4);
+        this.add(dis);
+        
+        
+		this.getContentPane().setBackground(Color.WHITE);
+        
+        
+        this.setVisible(true);
+        
 	}
+	
+	
+	public void addX(String x) {
+		this.x += x;
+	}
+	
+	public void addY(String y) {
+		this.y += y;
+	}
+	
+	
+	public String getAnswer(String x, String y, String expres) {
+		
+		
+		if(expres == "+") {
+			this.answer = Integer.toString(Integer.parseInt(x) + Integer.parseInt(y));
+			
+		}
+		
+		else if(expres == "-") {
+			this.answer = Integer.toString(Integer.parseInt(x) - Integer.parseInt(y));
+		}
+		
+		else if(expres == "*") {
+			this.answer = Integer.toString(Integer.parseInt(x) * Integer.parseInt(y));
+		}
+		
+		else if(expres == "/") {
+			if(y=="0")
+				this.answer = "Divide by 0 Error!";
+			else {
+				this.answer = Integer.toString(Integer.parseInt(x) / Integer.parseInt(y));
+			}
+
+		}
+		else {
+			this.answer = "EXPRES WAS SOMEHOW NOTHING!";
+		}
+		return this.answer;
+			
+	}
+	
+	
 }
--- Numpad.java
+++ Numpad.java
@@ -1,5 +1,6 @@
 import java.awt.Color;
 import java.awt.GridBagConstraints;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -8,9 +9,9 @@ import javax.swing.*;
 
 public class Numpad extends JPanel{
 	
-    public static final int WIDTH = (4 * Calculator.WIDTH/5), HEIGHT = ( 3 * Calculator.HEIGHT/4);
+    public static final int WIDTH = Calculator.WIDTH, HEIGHT = ( 3 * Calculator.HEIGHT/4);
 	
-	private ArrayList<JButton> numbers;
+	private ArrayList<JButton> buttons;
 	private ArrayList<String> buttonNames;
 	
 	
@@ -18,18 +19,28 @@ public class Numpad extends JPanel{
 	 
 	public Numpad() {
 		
-		this.numbers = new ArrayList<JButton>();
+		this.buttons = new ArrayList<JButton>();
+
 		this.buttonNames = new ArrayList<String>();
+		
+		this.buttonNames.add("*");
 		for(int i = 9 ; i > 0; i--) {
+			if(i == 6) {
+				this.buttonNames.add("/");
+			}
+			if(i == 3) {
+				this.buttonNames.add("-");
+			}
 			this.buttonNames.add("" + i);
 		}
+		this.buttonNames.add("+");
 		this.buttonNames.add("=");
 		this.buttonNames.add(".");
 		this.buttonNames.add("0");
 		
         this.setLayout(null);
-        this.setSize(4 * Calculator.WIDTH/5, 3 * Calculator.HEIGHT/4);
-        this.setBounds(0, Calculator.HEIGHT / 5, 4 * Calculator.WIDTH / 5, 3 * Calculator.HEIGHT / 4);
+        this.setSize(Calculator.WIDTH, 3 * Calculator.HEIGHT / 4);
+        this.setBounds(0, Calculator.HEIGHT / 5, Calculator.WIDTH, 3 * Calculator.HEIGHT / 4);
         this.setBackground(Color.BLACK);
 		
     	JButton button;
@@ -37,16 +48,24 @@ public class Numpad extends JPanel{
         int k = 0;
     	
         for(int i = 0; i < 4; i++) {
-        	for(int j = 2; j >= 0; j--) {
+        	for(int j = 3; j >= 0; j--) {
 	        	button = new JButton(this.buttonNames.get(k));
-	        	button.setSize( Numpad.WIDTH / 3, Numpad.HEIGHT / 4);
-	        	button.setBounds( ((j%3) * Numpad.WIDTH) / 3, ((i%4) * Numpad.HEIGHT) / 4, Numpad.WIDTH / 3, Numpad.HEIGHT / 4);
-		        this.numbers.add(button);
+	        	//button.setSize( Numpad.WIDTH / 3, Numpad.HEIGHT / 4);
+	        	button.setSize( Numpad.WIDTH / 4, Numpad.HEIGHT / 4);
+	        	//button.setBounds( ((j%3) * Numpad.WIDTH) / 3, ((i%4) * Numpad.HEIGHT) / 4, Numpad.WIDTH / 3, Numpad.HEIGHT / 4);
+	        	button.setBounds( ((j%4) * Numpad.WIDTH) / 4, ((i%4) * Numpad.HEIGHT) / 4, Numpad.WIDTH / 4, Numpad.HEIGHT / 4);
+		        this.buttons.add(button);
 		        this.add(button);
 		        k++;
         	}
         }
 		
+	}
+	
+	public  void setListener(ActionListener a) {
+		for(int i = 0; i < 16; i++) {
+			this.buttons.get(i).addActionListener(a);
+		}
 	}
 	
 	
